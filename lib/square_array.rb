d3a2454d626ebf3c6aa4e4f0c5749a17d638505a
Alex Marz <alexhmarz@gmail.com>
2020-11-01 21:49:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
+
 def square_array(array)
-  # your code here
+  counter = 0 
+  new_array = []
+  while array[counter] do
+    
+    counter += 1 
+    new_array << array[counter]**2
+  
+  return new_array
+end
 end
