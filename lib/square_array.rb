bf16e4a89a3b08b39d57fa51f64dbaec5ae87567
Alex Marz <alexhmarz@gmail.com>
2020-11-01 21:50:08 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,10 +3,8 @@ def square_array(array)
   counter = 0 
   new_array = []
   while array[counter] do
-    
-    counter += 1 
     new_array << array[counter]**2
-  
+    counter += 1
   return new_array
 end
 end
